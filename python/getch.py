665001fb32ce320cc710f72b320c56e20b2a9e21
Nick Stevens <Nick.Stevens@digi.com>
2015-07-27 16:07:19 -0500

    Python getch

---
+++ python/getch.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import termios
+import tty
+
+def get_ch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+for _ in range(10):
+    val = get_ch()
+    print "Got: " + val
