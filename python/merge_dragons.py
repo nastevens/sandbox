36509899ca1d64077bc2b85c25e54606c37e3900
Nick Stevens <nick.stevens@smartthings.com>
2020-02-08 13:15:56 -0600

    Fixup merge_dragons.py

--- python/merge_dragons.py
+++ python/merge_dragons.py
@@ -1,4 +1,5 @@
-import sys
+import argparse
+
 
 def merge(level, count, minlevel=0):
     if level <= minlevel:
@@ -10,4 +11,17 @@ def merge(level, count, minlevel=0):
 
 
 if __name__ == '__main__':
-    print(merge(*(int(arg) for arg in sys.argv[1:])))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('level', type=int, help='Desired level')
+    parser.add_argument('count', type=int, help='Desired number')
+    parser.add_argument(
+        'minlevel', type=int, default=0, nargs='?', help='Minimum level to use'
+    )
+    args = parser.parse_args()
+    assert args.level >= 0
+    assert args.level >= args.minlevel
+    assert args.level < 20
+    assert args.count >= 0
+    assert args.minlevel >= 0
+    assert args.minlevel < 20
+    print(merge(args.level, args.count, args.minlevel))
